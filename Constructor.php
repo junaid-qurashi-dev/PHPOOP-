8ab3453d53896457e39e92f982047b4081c6b0b8
junaid <mohmmadjunaidqurashi@gmail.com>
2026-04-11 09:53:16 +0530

    default value set ki

--- Constructor.php
+++ Constructor.php
@@ -3,11 +3,11 @@
 class person
 {
     // Default Value Set ki Huumnne jab koi value na ho to yeh value set ho jaye//
-    public $name = "Junaid";
-    public $age = 21;
+    public $name;
+    public $age;
 
     // Constructor Function use kiya yeh object bante hi automatically call hota hai //
-    function __construct($name, $age)
+    function __construct($name = "Ali", $age = 19)
     {
         $this->name = $name;
         $this->age = $age;
@@ -17,7 +17,7 @@ class person
         echo $this->name . " - " . $this->age;
     }
 }
-$p1 = new person("Junaid ", 21);
+$p1 = new person();
 // $p1->name = "YAHOBABA";
 // $p1->age = 19;
 $p1->show();
