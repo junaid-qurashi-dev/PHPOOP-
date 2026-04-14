5b04f6b981dc8469676cb047c9e4c5f9625a8f44
junaid <mohmmadjunaidqurashi@gmail.com>
2026-04-14 11:15:42 +0530

    overriding method

---
+++ overriding.php
@@ -0,0 +1,21 @@
+<?php
+
+class base
+{
+    public $name = "Junaid";
+
+    function calc($a, $b)
+    {
+        return $a * $b;
+    }
+}
+class derived extends base
+{
+    public $name = "Quraishi";
+    function calc($a, $b)
+    {
+        return $a + $b;
+    }
+}
+$obj = new base();
+echo $obj->calc(21, 21);
